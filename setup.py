2158ad94a61181a44b987ee4cdb56de7b084d440
junetigerlee <junetigerlee@gmail.com>
2017-07-25 14:41:07 +0800

    first commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 import sys
 from setuptools import setup, find_packages
 
-NAME = "python-wso2-apim-publisherclient"
+NAME = "python-wso2-apim-tokenclient"
 VERSION = "0.11.0"
 # To install the library, run the following
 #
